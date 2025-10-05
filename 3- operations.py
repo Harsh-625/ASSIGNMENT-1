fb1d4097b8e8907992c368213f10a9bc390187f0
Harsh-625 <anupam6879@gmail.com>
2025-10-05 13:26:35 +0530

    Add files via upload
    here, the task 1 is operations.py and task 2 is greetings.py

---
+++ 3- operations.py
@@ -0,0 +1,12 @@
+n1 =input('enter the first number:- ')
+n1=float(n1)
+n2 =input('enter the second number:- ')
+n2=float(n2)
+a=n1+n2
+b=n1-n2
+c=n1*n2
+d=n1/n2
+print('addition:-',a)
+print('subtraction:-',b)
+print('multiplication:-',c)
+print('division:-',d)
